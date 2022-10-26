7285a695edf7cc3237f82e7e0db94dd256cd1c3d
Xanthus Wong <wong.xanthus@bcthk.com>
2022-10-26 12:38:14 +0800

    - authentication

---
+++ SleekFlow.Web.WebAPI/DTOs/WebAuth/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿namespace SleekFlow.Web.WebAPI.DTOs.Login
+{
+    public class LoginDto
+    {
+        /// <summary>
+        /// username
+        /// </summary>
+        /// <example>user1</example>
+        public string UserName { get; set; }
+        /// <summary>
+        /// password
+        /// </summary>
+        /// <example>P@ssw0rd</example>
+        public string Password { get; set; }
+    }
+}
--- SleekFlow.Web.WebAPI/Program.cs
+++ SleekFlow.Web.WebAPI/Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
 using System.Reflection;
+using System.Text;
 
 namespace SleekFlow.Web.WebAPI
 {
@@ -11,15 +19,69 @@ namespace SleekFlow.Web.WebAPI
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(configure =>
+            {
+                configure.OutputFormatters.RemoveType<HttpNoContentOutputFormatter>();
+
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                configure.Filters.Add(new AuthorizeFilter(policy));
+            });
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
             builder.Services.AddSwaggerGen(setupAction =>
             {
                 setupAction.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+
+                setupAction.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = JwtBearerDefaults.AuthenticationScheme,
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JSON Web Token based security",
+                });
+
+                setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
             builder.Services.AddAutoMapper(typeof(Program));
             builder.Services.AddDbContext<DAL.DbContexts.SleekFlowWebDbContext>(optionsAction => { });
+
+            // Add JWT configuration
+            builder.Services.AddAuthentication(o =>
+            {
+                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                o.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+            builder.Services.AddAuthorization();
+            builder.Services.AddEndpointsApiExplorer();
 
             builder.Services.AddTransient<Todos.TodoService>();
 
@@ -34,8 +96,8 @@ namespace SleekFlow.Web.WebAPI
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
